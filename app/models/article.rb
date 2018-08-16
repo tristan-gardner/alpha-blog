d7032d19c4828cd317ad99c98634882c5c92d2ab
Tristan Gardner <trgardner123@gmail.com>
2018-08-16 16:04:19 +0000

    created articles table model validation

---
+++ app/models/article.rb
@@ -0,0 +1,6 @@
+class Article < ActiveRecord::Base
+    #add validations in this file
+    validates :title, presence: true, length: {minimum: 3, maximum: 50}
+    validates :description, presence: true, length: {minimum: 10, maximum: 300}
+    
+end
