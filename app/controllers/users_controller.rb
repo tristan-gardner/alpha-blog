246003080d5d1e4a97fad11090f1b98b37696e4b
Tristan Gardner <trgardner123@gmail.com>
2018-09-27 21:02:40 +0000

    login / signup buttons

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
    
+   before_action :set_user, only: [:edit, :update, :show]
+   
    def index
       @users = User.paginate(page: params[:page], per_page: 5)
    end
@@ -18,11 +20,11 @@ class UsersController < ApplicationController
    end
    
    def edit
-      @user = User.find(params[:id])
+      
    end
    
    def update
-      @user = User.find(params[:id])
+      
       if @user.update(user_params)
          flash[:success] = "Account Updated"
          redirect_to articles_path
@@ -32,13 +34,17 @@ class UsersController < ApplicationController
    end
    
    def show
-      @user = User.find(params[:id])
+      
       @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
    end
    
    private
    def user_params
       params.require(:user).permit(:username,:email,:password) 
+   end
+   
+   def set_user
+      @user = User.find(params[:id])
    end
    
 end
